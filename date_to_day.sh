f98219edad12de8e5e9d2d8577e02df4af91e198
jumahmohammad <jumah.mohammad@gmail.com>
2020-01-20 23:39:35 +0000

    fixed bug using date --date

--- date_to_day.sh
+++ date_to_day.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-DAY=""
-while [[ ! $DAY =~ ^[0-9]+$ ]]  ; do
+day=""
+while [[ ! $day =~ ^[0-9]+$ ]]  ; do
     echo Please enter day in numbers. Example 10
-read DAY
+read day
 done
-MONTH=""
+month=""
 #while [[ ! $MONTH =~ ^[0-9]+$ ]]; do
     echo Please enter month. Example Jun or 06.
-read MONTH
-YEAR=""
-while [[ ! $YEAR =~ ^[0-9]+$ ]]; do
+read month
+year=""
+while [[ ! $year =~ ^[0-9]+$ ]]; do
     echo Please enter year. Example 1990.
-read YEAR
+read year
 done
-new="$MONTH $DAY $YEAR"
-echo "On this date the day was a "`date -j -v"$MONTH"m -v"$DAY"d -v"$YEAR"y '+%A'`""
+new=$(date -d "$year-$month-$day" '+%A')
+echo "On this date the day was a"
+echo $new
